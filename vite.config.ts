ab64ed3f66c3343730968f8b9792a85929ae4dcd
Autade Dhiraj Laxman <autade.laxman@mca.christuniversity.in>
2025-11-03 18:58:09 +0530

    Updated Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BASE_URL || '/Flight-Explorer-App-Assignment--Lead-Walnut',
   test: {
     environment: 'jsdom',
     globals: true,
